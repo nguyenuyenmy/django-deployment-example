22debc857b95d98aa6637fdadfbae20961c78eb1
Uyen My NGUYEN <uyenmy.nguyen@OFFY.LOCAL>
2022-05-11 10:24:01 +0700

    modifications

---
+++ my_video_rental/videos/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['release_year', 'title', 'length']
+    search_fields = ['title']
+    list_filter = ['release_year', 'length']
+    list_display = ['title', 'release_year', 'length']
+    list_editable = ['length']
+
+admin.site.register(models.Customer)
+admin.site.register(models.Movie, MovieAdmin)
